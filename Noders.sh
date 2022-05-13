edbe5a6bef342c0c8dad5812b6c779aa5cdbe057
SeptimA <94483941+AlekseyMoskalev1@users.noreply.github.com>
2022-05-13 13:11:36 +0300

    Update Noders.sh

--- Noders.sh
+++ Noders.sh
@@ -2,8 +2,8 @@
  
 echo -e " MMMM \033[5;34m ___      ___   ____    ________   __________ ________      ____   \033[0m MMMM __________ __________        _      ___       ___ "
 echo -e " MM   \033[5;34m  MM\      M   6MMMMb    MMMMMMMb.  MMMMMMMMM  MMMMMMMb.   6MMMMb\ \033[0m   MM MMMMMMMMMM  MMMMMMMMM       dM.      MMb     dMM  "
-echo -e " MM   \033[5;34m  MMM\     M  8P    Y8   MM     Mb  MM         MM     Mb  6M'      \033[0m   MM     MM      MM             ,MMb      MMM     PMM  "
-echo -e " MM   \033[5;34m  M\MM\    M 6M      Mb  MM     MM  MM         MM     MM  MM       \033[0m   MM     MM      MM             d'YM.     M`Mb   d'MM  "
+echo -e " MM   \033[5;34m  MMM\     M  8P    Y8   MM     Mb  MM         MM     Mb  6M       \033[0m   MM     MM      MM             ,MMb      MMM     PMM  "
+echo -e " MM   \033[5;34m  M\MM\    M 6M      Mb  MM     MM  MM         MM     MM  MM       \033[0m   MM     MM      MM             d'YM.     M Mb   d MM  "
 echo -e " MM   \033[5;34m  M \MM\   M MM      MM  MM     MM  MM         MM     MM  YM.      \033[0m   MM     MM      MM            ,P  Mb     M YM. ,P MM  "
 echo -e " MM   \033[5;34m  M  \MM\  M MM      MM  MM     MM  MMMMMMM    MM    .M9   YMMMMb  \033[0m   MM     MM      MMMMMMM       d   YM.    M  Mb d  MM  "
 echo -e " MM   \033[5;34m  M   \MM\ M MM      MM  MM     MM  MM         MMMMMMM9         Mb \033[0m   MM     MM      MM           ,P    Mb    M  YM.P  MM  "
